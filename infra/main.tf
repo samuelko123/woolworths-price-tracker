f234a0fbed0cbdfb86c95f1dc85b8eef05f39782
Samuel Ko <koshunyin@gmail.com>
2025-04-18 21:46:40 +1000

    add linebreak

--- infra/main.tf
+++ infra/main.tf
@@ -1,6 +1,7 @@
 ###############################
 # Price Fetcher
 ###############################
+
 resource "aws_iam_role" "woolworths_price_fetcher_role" {
   assume_role_policy = jsonencode({
     Version = "2012-10-17",
