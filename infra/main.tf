b0f35d4ea1b6b9ebcb6e1186fb785f52e6307911
Samuel Ko <koshunyin@gmail.com>
2025-04-18 20:59:31 +1000

    create scheduler

--- infra/main.tf
+++ infra/main.tf
@@ -29,3 +29,71 @@ resource "aws_lambda_function" "woolworths_price_fetcher" {
     log_format = "JSON"
   }
 }
+
+resource "aws_iam_role" "eventbridge_scheduler_invoke_woolworths_price_fetcher_role" {
+  name = "eventbridge_scheduler_invoke_woolworths_price_fetcher_role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Effect = "Allow",
+      Principal = {
+        Service = "scheduler.amazonaws.com"
+      },
+      Action = "sts:AssumeRole"
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "eventbridge_scheduler_invoke_woolworths_price_fetcher_role_polixy_attachment" {
+  role       = aws_iam_role.eventbridge_scheduler_invoke_woolworths_price_fetcher_role.name
+  policy_arn = aws_iam_policy.allow_lambda_invoke.arn
+}
+
+resource "aws_iam_role" "woolworths_price_fetch_scheduler_role" {
+  name = "woolworths_price_fetch_scheduler_role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Action    = "sts:AssumeRole",
+        Principal = { Service = "lambda.amazonaws.com" },
+        Effect    = "Allow",
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "woolworths_price_fetch_scheduler_role_policy_attachment" {
+  role       = aws_iam_role.woolworths_price_fetch_scheduler_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+
+resource "aws_iam_role_policy_attachment" "woolworths_price_fetch_scheduler_role_policy_attachment_scheduler" {
+  role       = aws_iam_role.woolworths_price_fetch_scheduler_role.name
+  policy_arn = aws_iam_policy.allow_scheduler_creation.arn
+}
+
+resource "aws_iam_role_policy_attachment" "woolworths_price_fetch_scheduler_role_policy_attachment_passrole" {
+  role       = aws_iam_role.woolworths_price_fetch_scheduler_role.name
+  policy_arn = aws_iam_policy.allow_passrole_policy.arn
+}
+
+resource "aws_lambda_function" "woolworths_price_fetch_scheduler" {
+  filename         = "${path.module}/../dist/woolworths_price_fetch_scheduler.zip"
+  function_name    = "woolworths_price_fetch_scheduler"
+  role             = aws_iam_role.woolworths_price_fetch_scheduler_role.arn
+  handler          = "woolworths_price_fetch_scheduler.handler"
+  runtime          = "nodejs18.x"
+  source_code_hash = filebase64sha256("${path.module}/../dist/woolworths_price_fetch_scheduler.zip")
+
+  logging_config {
+    log_format = "JSON"
+  }
+
+  environment {
+    variables = {
+      TARGET_LAMBDA_ARN  = aws_lambda_function.woolworths_price_fetcher.arn
+      SCHEDULER_ROLE_ARN = aws_iam_role.eventbridge_scheduler_invoke_woolworths_price_fetcher_role.arn
+    }
+  }
+}
---
+++ src/lambdas/woolworths_price_fetch_scheduler.ts
@@ -0,0 +1,49 @@
+import {
+  SchedulerClient,
+  CreateScheduleCommand,
+} from "@aws-sdk/client-scheduler";
+
+const client = new SchedulerClient({ region: "ap-southeast-2" });
+
+export const handler = async () => {
+  const targetLambdaArn = process.env.TARGET_LAMBDA_ARN;
+  if (!targetLambdaArn) {
+    console.error("TARGET_LAMBDA_ARN is not set");
+    return;
+  }
+
+  const schedulerRoleArn = process.env.SCHEDULER_ROLE_ARN;
+  if (!schedulerRoleArn) {
+    console.error("SCHEDULER_ROLE_ARN is not set");
+    return;
+  }
+
+  const futureTime = new Date(Date.now() + 2 * 60 * 1000) // +2 minutes
+    .toISOString()
+    .replace(/\.\d{3}Z$/, "");
+  const scheduleName = `one-off-${Date.now()}`;
+
+  const command = new CreateScheduleCommand({
+    Name: scheduleName,
+    ScheduleExpression: `at(${futureTime})`,
+    FlexibleTimeWindow: { Mode: "OFF" },
+    Target: {
+      Arn: targetLambdaArn,
+      RoleArn: schedulerRoleArn,
+      Input: JSON.stringify({
+        productId: "1234567890",
+      }),
+    },
+    ActionAfterCompletion: "DELETE",
+  });
+
+  try {
+    const response = await client.send(command);
+    console.log({
+      message: "Scheduled successfully",
+      response,
+    });
+  } catch (error) {
+    console.error("Failed to schedule:", error);
+  }
+};
