3fb9d6ee28931996e6e3d4526522a3aadc71d39d
Samuel Ko <koshunyin@gmail.com>
2025-04-18 12:10:41 +1000

    convert to Typescript

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,8 @@ rm -rf ./dist
 mkdir ./dist
 
 # package the lambda function into zip files
-zip ./dist/woolworths_price_fetcher.zip ./src/lambdas/woolworths_price_fetcher.js --junk-paths --quiet
+pnpm --dir ./src/lambdas build
+zip ./dist/woolworths_price_fetcher.zip ./src/lambdas/dist/woolworths_price_fetcher.js --junk-paths --quiet
 
 # apply infrastructure changes
 terraform -chdir=infra init
