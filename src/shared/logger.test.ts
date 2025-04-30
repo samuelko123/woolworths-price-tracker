2bf8cd50759ba536cc52de109a20d7caeea7696b
Samuel Ko <koshunyin@gmail.com>
2025-05-01 00:17:24 +1000

    make `logger.error` handle `unknown`

--- src/shared/logger.test.ts
+++ src/shared/logger.test.ts
@@ -17,15 +17,41 @@ describe("logger", () => {
     });
   });
 
-  it("calls console.error", () => {
+  it("passes an error to console.error with error message and stacktrace", () => {
     const spy = vi.spyOn(console, "error").mockImplementation(() => {});
 
-    logger.error({
-      message: "something went wrong",
-    });
+    logger.error(new Error("something went wrong"));
 
     expect(spy).toHaveBeenCalledWith({
+      name: "Error",
       message: "something went wrong",
+      stack: expect.any(String),
     });
+  });
+
+  it("passes an object to console.error", () => {
+    const spy = vi.spyOn(console, "error").mockImplementation(() => {});
+
+    logger.error({ hello: "world" });
+
+    expect(spy).toHaveBeenCalledWith({
+      hello: "world",
+    });
+  });
+
+  it("passes a string to console.error", () => {
+    const spy = vi.spyOn(console, "error").mockImplementation(() => {});
+
+    logger.error("something went wrong");
+
+    expect(spy).toHaveBeenCalledWith("something went wrong");
+  });
+
+  it("passes a number to console.error", () => {
+    const spy = vi.spyOn(console, "error").mockImplementation(() => {});
+
+    logger.error(123);
+
+    expect(spy).toHaveBeenCalledWith(123);
   });
 });
--- src/shared/logger.ts
+++ src/shared/logger.ts
@@ -2,7 +2,15 @@ export const logger = {
   info: (data: object) => {
     console.info(data);
   },
-  error: (data: object) => {
-    console.error(data);
+  error: (error: unknown) => {
+    if (error instanceof Error) {
+      console.error({
+        name: error.name,
+        message: error.message,
+        stack: error.stack,
+      });
+    } else {
+      console.error(error);
+    }
   },
 };
