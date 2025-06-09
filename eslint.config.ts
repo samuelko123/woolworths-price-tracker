512cf298f4c66ec4fddc0593967b1a21e3bd2342
Samuel Ko <koshunyin@gmail.com>
2025-06-09 18:34:16 +1000

    set up eslint airbnb

---
+++ eslint.config.ts
@@ -0,0 +1,28 @@
+import tsPlugin from '@typescript-eslint/eslint-plugin';
+import tsParser from '@typescript-eslint/parser';
+import airbnbBaseConfig from 'eslint-config-airbnb-base';
+
+export default [
+  {
+    files: ['**/*.ts'],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        project: './tsconfig.json',
+        sourceType: 'module',
+        ecmaVersion: 2022,
+      },
+    },
+    plugins: {
+      '@typescript-eslint': tsPlugin,
+    },
+    rules: {
+      ...airbnbBaseConfig.rules,
+      ...tsPlugin.configs['eslint-recommended'].rules,
+      ...tsPlugin.configs['recommended'].rules,
+      'import/prefer-default-export': 'off',
+      'no-console': 'warn',
+      'react/jsx-filename-extension': 'off',
+    },
+  },
+];
