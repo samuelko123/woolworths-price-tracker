97e3b1f55739d2a0800c921fd9e876d8ed3b3868
Samuel Ko <koshunyin@gmail.com>
2025-06-21 09:46:06 +1000

    add `logDuration` to `category` feature

--- src/features/category/handler.test.ts
+++ src/features/category/handler.test.ts
@@ -4,7 +4,12 @@ import { handler } from "./handler";
 import { fetchAndQueueCategories } from "./service";
 
 vi.mock("./service");
-vi.mock("@/core/logger");
+vi.mock("@/core/logger", () => {
+  return {
+    logError: vi.fn(),
+    logDuration: vi.fn().mockImplementation(async (_, fn) => await fn()),
+  };
+});
 
 describe("handler", () => {
   it("returns 200 when success", async () => {
