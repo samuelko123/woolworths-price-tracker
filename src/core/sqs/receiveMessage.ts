2a107060a20d1908669efac3af5d16933bbbc7c9
Samuel Ko <koshunyin@gmail.com>
2025-06-24 00:33:25 +1000

    refactor `receiveMessage`

--- src/core/sqs/receiveMessage.ts
+++ src/core/sqs/receiveMessage.ts
@@ -1,4 +1,4 @@
-import { ReceiveMessageCommand } from "@aws-sdk/client-sqs";
+import { type Message, ReceiveMessageCommand } from "@aws-sdk/client-sqs";
 
 import { err, ok, type Result, tryCatch } from "@/core/result";
 
@@ -11,6 +11,17 @@ type SqsMessage = {
 
 type ReceiveMessage = (queueUrl: string) => Promise<Result<SqsMessage>>;
 
+const validateSqsMessage = (message: Message | undefined): Result<SqsMessage> => {
+  if (!message) return err(new Error("No messages received from the queue."));
+  if (!message.Body) return err(new Error("Body is missing from the message."));
+  if (!message.ReceiptHandle) return err(new Error("ReceiptHandle is missing from the message."));
+
+  return ok({
+    Body: message.Body,
+    ReceiptHandle: message.ReceiptHandle,
+  });
+};
+
 export const receiveMessage: ReceiveMessage = async (queueUrl) => {
   const command = new ReceiveMessageCommand({
     QueueUrl: queueUrl,
@@ -20,23 +31,5 @@ export const receiveMessage: ReceiveMessage = async (queueUrl) => {
   });
 
   const result = await tryCatch(() => client.send(command));
-  if (!result.success) return result;
-
-  const message = result.value.Messages?.[0];
-  if (!message) {
-    return err(new Error("No messages received from the queue."));
-  }
-
-  if (!message.Body) {
-    return err(new Error("Body is missing from the message."));
-  }
-
-  if (!message.ReceiptHandle) {
-    return err(new Error("ReceiptHandle is missing from the message."));
-  }
-
-  return ok({
-    Body: message.Body,
-    ReceiptHandle: message.ReceiptHandle,
-  });
+  return result.flatMap((res) => validateSqsMessage(res.Messages?.[0]));
 };
