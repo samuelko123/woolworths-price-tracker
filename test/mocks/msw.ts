b30140b2864e512ba371b8f0b32e957d487cbec9
Samuel Ko <koshunyin@gmail.com>
2025-06-27 00:29:13 +1000

    code styling

--- test/mocks/msw.ts
+++ test/mocks/msw.ts
@@ -2,12 +2,8 @@ import { setupServer } from "msw/node";
 
 export const testServer = setupServer();
 
-beforeAll(() => {
-  testServer.listen({ onUnhandledRequest: "warn" });
-});
-
+beforeAll(() => testServer.listen({ onUnhandledRequest: "warn" }));
 beforeEach(() => testServer.resetHandlers());
-
 afterAll(() => testServer.close());
 
 export { http, HttpResponse } from "msw";
