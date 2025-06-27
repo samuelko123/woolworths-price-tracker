5366c75a8c81415afd90a661ad22c41fb04c640a
Samuel Ko <koshunyin@gmail.com>
2025-06-27 22:07:53 +1000

    Refactor - Remove `strict` in `CategorySchema` (#59)

--- src/domain/category.ts
+++ src/domain/category.ts
@@ -4,6 +4,6 @@ export const CategorySchema = z.object({
   id: z.string(),
   urlName: z.string(),
   displayName: z.string(),
-}).strict();
+});
 
 export type Category = z.infer<typeof CategorySchema>;
