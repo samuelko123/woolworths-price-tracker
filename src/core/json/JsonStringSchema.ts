2a498baff0c77b355cda9e5e9ddb8c586dcf3b74
Samuel Ko <koshunyin@gmail.com>
2025-06-27 21:00:25 +1000

    code styling

--- src/core/json/JsonStringSchema.ts
+++ src/core/json/JsonStringSchema.ts
@@ -1,13 +1,14 @@
 import { z } from "zod";
 
-export const JsonStringSchema = z.string().transform((val, ctx) => {
-  try {
-    return JSON.parse(val);
-  } catch {
-    ctx.addIssue({
-      code: z.ZodIssueCode.custom,
-      message: "Invalid JSON string",
-    });
-    return z.NEVER;
-  }
-});
+export const JsonStringSchema = z.string()
+  .transform((val, ctx) => {
+    try {
+      return JSON.parse(val);
+    } catch {
+      ctx.addIssue({
+        code: z.ZodIssueCode.custom,
+        message: "Invalid JSON string",
+      });
+      return z.NEVER;
+    }
+  });
