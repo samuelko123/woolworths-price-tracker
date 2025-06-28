6513d5aed6e84206f27364c3b8d98b6d1be831d4
Samuel Ko <koshunyin@gmail.com>
2025-06-28 10:24:53 +1000

    code styling

--- src/core/config/getWoolworthsBaseUrl.ts
+++ src/core/config/getWoolworthsBaseUrl.ts
@@ -2,6 +2,7 @@ import { ResultAsync } from "../result";
 
 export const getWoolworthsBaseUrl = () => {
   const url = process.env.WOOLWORTHS_BASE_URL;
+
   return url
     ? ResultAsync.ok(url)
     : ResultAsync.err(new Error("Missing WOOLWORTHS_BASE_URL"));
