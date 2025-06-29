ff9f0d5671805023df63932d9249fb91b59141d7
Samuel Ko <koshunyin@gmail.com>
2025-06-29 19:27:07 +1000

    fix `parseWithSchema`

--- src/core/validation/parseWithSchema.ts
+++ src/core/validation/parseWithSchema.ts
@@ -4,7 +4,7 @@ import { type ZodType, type ZodTypeDef } from "zod";
 export const parseWithSchema = <T>(
   schema: ZodType<T, ZodTypeDef, unknown>,
   data: unknown,
-): ResultAsync<T> => {
+): ResultAsync<T, Error> => {
   const parsed = schema.safeParse(data);
   return parsed.success
     ? okAsync(parsed.data)
