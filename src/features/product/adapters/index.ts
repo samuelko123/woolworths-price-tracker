ed4ff3e271e02a2757ad71e65bc35912ceaf0d0c
Samuel Ko <koshunyin@gmail.com>
2025-07-02 00:24:19 +1000

    update `saveProducts`

--- src/features/product/adapters/index.ts
+++ src/features/product/adapters/index.ts
@@ -1,3 +1,3 @@
 export { fetchProducts } from "./fetchProducts";
 export { parseCategory } from "./parseCategory";
-export { saveProducts } from "./saveProducts";
+export { createSaveProducts } from "./saveProducts";
--- src/features/product/adapters/saveProducts.test.ts
+++ src/features/product/adapters/saveProducts.test.ts
@@ -1,91 +1,87 @@
-import { DynamoDBDocumentClient, PutCommand } from "@aws-sdk/lib-dynamodb";
+import { PutCommand } from "@aws-sdk/lib-dynamodb";
 import { mockClient } from "aws-sdk-client-mock";
 
+import { createDocumentClient } from "@/core/dynamodb";
 import { expectErr, expectOk } from "@/tests/helpers";
 
-import { saveProducts } from "./saveProducts";
+import { createSaveProducts } from "./saveProducts";
 import { mockProduct1, mockProduct2 } from "./saveProducts.test.data";
 
-const client = mockClient(DynamoDBDocumentClient);
-
 describe("saveProducts", () => {
+  const tableName = "products";
+
+  const client = createDocumentClient();
+  const stub = mockClient(client);
+
+  const products = [mockProduct1, mockProduct2];
+
   beforeEach(() => {
-    client.reset();
+    stub.reset();
   });
 
   it("sends one PutCommand per product", async () => {
-    client.resolves({});
+    stub.on(PutCommand).resolves({});
 
-    const result = await saveProducts([mockProduct1, mockProduct2]);
+    const saveProducts = createSaveProducts(client);
+    const result = await saveProducts(products);
 
     expectOk(result);
 
-    const calls = client.calls();
+    const calls = stub.calls();
     expect(calls).toHaveLength(2);
 
     expect(calls[0].args[0]).toBeInstanceOf(PutCommand);
-    expect(calls[0].args[0]).toEqual(
-      expect.objectContaining({
-        input: {
-          TableName: "products",
-          Item: mockProduct1,
-        },
-      }),
-    );
+    expect(calls[0].args[0].input).toEqual({
+      TableName: tableName,
+      Item: mockProduct1,
+    });
+
     expect(calls[1].args[0]).toBeInstanceOf(PutCommand);
-    expect(calls[1].args[0]).toEqual(
-      expect.objectContaining({
-        input: {
-          TableName: "products",
-          Item: mockProduct2,
-        },
-      }),
-    );
+    expect(calls[1].args[0].input).toEqual({
+      TableName: tableName,
+      Item: mockProduct2,
+    });
   });
 
   it("does not send any command when receiving empty array", async () => {
-    client.resolves({});
+    stub.resolves({});
 
+    const saveProducts = createSaveProducts(client);
     const result = await saveProducts([]);
 
     expectOk(result);
-    expect(client.calls()).toHaveLength(0);
+    expect(stub.calls()).toHaveLength(0);
   });
 
   it("returns error if any PutCommand fails", async () => {
-    client.rejects(new Error("DynamoDB failure"));
+    const error = new Error("DynamoDB failure");
+    stub.rejects(error);
 
+    const saveProducts = createSaveProducts(client);
     const result = await saveProducts([mockProduct1]);
 
     expectErr(result);
-    expect(result.error.message).toBe("DynamoDB failure");
+    expect(result.error).toBe(error);
   });
 
   it("does not send subsequent products if one fails", async () => {
-    client
-      .on(PutCommand, {
-        TableName: "products",
-        Item: mockProduct1,
-      })
+    stub
+      .on(PutCommand)
       .rejects(new Error("Product 1 failed"));
 
+    const saveProducts = createSaveProducts(client);
     const result = await saveProducts([mockProduct1, mockProduct2]);
 
     expectErr(result);
     expect(result.error.message).toBe("Product 1 failed");
 
-    // Only the first call should be made
-    const calls = client.calls();
+    const calls = stub.calls();
     expect(calls).toHaveLength(1);
 
     expect(calls[0].args[0]).toBeInstanceOf(PutCommand);
-    expect(calls[0].args[0]).toEqual(
-      expect.objectContaining({
-        input: {
-          TableName: "products",
-          Item: mockProduct1,
-        },
-      }),
-    );
+    expect(calls[0].args[0].input).toEqual({
+      TableName: tableName,
+      Item: mockProduct1,
+    });
   });
 });
