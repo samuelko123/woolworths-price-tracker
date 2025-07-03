92aadebaa2be6449b5986ce8a4d284d6072cc75b
Samuel Ko <koshunyin@gmail.com>
2025-07-04 00:40:47 +1000

    extract `isSpecialCategory`

--- src/features/category/domain/excludeSpecialCategories.ts
+++ src/features/category/domain/excludeSpecialCategories.ts
@@ -1,8 +1,9 @@
 import { type Category } from "./category";
 
+const isSpecialCategory = (category: Category): boolean => {
+  return category.id === "specialsgroup" || category.urlName === "front-of-store";
+};
+
 export const filterCategories = (categories: Category[]): Category[] => {
-  return categories.filter(
-    (category) =>
-      category.id !== "specialsgroup" && category.urlName !== "front-of-store",
-  );
+  return categories.filter((category) => !isSpecialCategory(category));
 }; 
