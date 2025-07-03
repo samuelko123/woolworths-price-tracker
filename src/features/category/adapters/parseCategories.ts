81d8013bfb07224fb40ed34804aa4f07f7dfcb96
Samuel Ko <koshunyin@gmail.com>
2025-07-04 01:07:09 +1000

    update `parseCategories` to not use `parseWithSchema`

--- src/features/category/adapters/parseCategories.ts
+++ src/features/category/adapters/parseCategories.ts
@@ -1,8 +1,12 @@
-import { parseWithSchema } from "@/core/validation";
+import { errAsync, okAsync } from "neverthrow";
 
 import { type ParseCategories } from "../application/ports";
 import { CategoriesDTOSchema } from "./parseCategories.schema";
 
-export const parseCategories: ParseCategories = (data) => {
-  return parseWithSchema(CategoriesDTOSchema, data);
+export const parseCategories: ParseCategories = (data: unknown) => {
+  const parsed = CategoriesDTOSchema.safeParse(data);
+
+  return parsed.success
+    ? okAsync(parsed.data)
+    : errAsync(parsed.error);
 };
