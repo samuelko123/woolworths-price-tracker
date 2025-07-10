2334eb3a81c2620204aab0dda3561f9f4e0d704c
Samuel Ko <koshunyin@gmail.com>
2025-07-10 18:48:25 +1000

    remove `FetchProducts` from exports

--- src/features/product/index.ts
+++ src/features/product/index.ts
@@ -1,2 +1,2 @@
-export { DeleteMessage, FetchProducts, SaveProducts } from "./application/use-cases/importProducts.ports";
+export { DeleteMessage, SaveProducts } from "./application/use-cases/importProducts.ports";
 export { Product } from "./domain/product";
