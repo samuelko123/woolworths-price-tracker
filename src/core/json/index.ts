d369b511c8bd5cb1ccd421edc553921fa96b2a3c
Samuel Ko <koshunyin@gmail.com>
2025-07-10 20:48:02 +1000

    Refactor - Move `JsonStringSchema` to `@/core/validation` (#77)

--- src/core/json/index.ts
+++
@@ -1,1 +0,0 @@
-export { JsonStringSchema } from "./JsonStringSchema";
--- src/features/category/application/services/parseCategory.ts
+++ src/features/category/application/services/parseCategory.ts
@@ -1,7 +1,6 @@
 import { type ResultAsync } from "neverthrow";
 
-import { JsonStringSchema } from "@/core/json";
-import { parseWithSchema } from "@/core/validation";
+import { JsonStringSchema, parseWithSchema } from "@/core/validation";
 import { type SqsMessage } from "@/gateways/sqs";
 
 import { type Category, CategorySchema } from "../../domain/category";
