c2c2bda16dc6a8325cca8119a4cf12f0522b7c1c
Samuel Ko <koshunyin@gmail.com>
2025-07-10 17:06:14 +1000

    move `dynamodb` to `gateways`

--- src/features/product/presentation/handler.ts
+++ src/features/product/presentation/handler.ts
@@ -1,7 +1,7 @@
 import { getCategoryQueueUrl } from "@/core/config";
-import { createDynamoDBDocumentClient, saveProductsWith } from "@/core/dynamodb";
 import { logDuration, logError, logInfo } from "@/core/logger";
 import { deleteMessage, receiveMessage } from "@/core/sqs";
+import { createDynamoDBDocumentClient, saveProductsWith } from "@/gateways/dynamodb";
 import { createApiClient, fetchProductsWith } from "@/gateways/woolworths";
 
 import { importProducts } from "../application/use-cases/importProducts";
